98ec845c92ab0c9905e32775667ee935d9d8da64
Miikhaaeel <fadowomichael1@gmail.com>
2021-07-18 01:09:02 +0100

    calculator brain

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ class BMICalculator extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => InputPage(),
-       // '/resultPage': (context) => ResultPage(),
       },
     );
   }
